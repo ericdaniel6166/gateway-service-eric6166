e2861bb2a1cac9105999ca7dd982bff7ad38da61
eric6166 <ericnguyen6166@gmail.com>
2024-05-02 19:57:23 +0700

    commit

--- src/main/java/com/eric6166/gateway/config/SecurityConfig.java
+++ src/main/java/com/eric6166/gateway/config/SecurityConfig.java
@@ -3,11 +3,17 @@ package com.eric6166.gateway.config;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.InMemoryReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -17,6 +23,7 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 public class SecurityConfig {
 
     SecurityProps securityProps;
+    OAuth2ClientProperties oAuth2ClientProperties;
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
@@ -27,10 +34,43 @@ public class SecurityConfig {
                         .pathMatchers(securityProps.getSkipUrls()).permitAll()
                         .anyExchange().authenticated())
                 .oauth2Login(Customizer.withDefaults())
+//                .oauth2Login(oauth2 -> oauth2
+//                        .authenticationMatcher(new PathPatternParserServerWebExchangeMatcher("/login/oauth2/code/{registrationId}")))
+//                .oauth2Login(oauth2 -> oauth2
+//                        .authenticationMatcher(new PathPatternParserServerWebExchangeMatcher("/login/oauth2/callback/{registrationId}")))
                 .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
                 .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
                 .oauth2Client(Customizer.withDefaults())
                 .logout(Customizer.withDefaults())
+                .build();
+    }
+
+
+    @Bean
+    public ReactiveClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryReactiveClientRegistrationRepository(this.microservicesAuthClientRegistration());
+    }
+
+    private ClientRegistration microservicesAuthClientRegistration() {
+        var keycloak = oAuth2ClientProperties.getProvider().get("keycloak");
+        var microservicesAuthClient = oAuth2ClientProperties.getRegistration().get("microservices-auth-client");
+
+        return ClientRegistration.withRegistrationId("microservices-auth-client")
+                .clientId(microservicesAuthClient.getClientId())
+                .clientSecret(microservicesAuthClient.getClientSecret())
+//                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .clientAuthenticationMethod(new ClientAuthenticationMethod(microservicesAuthClient.getClientAuthenticationMethod()))
+//                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .authorizationGrantType(new AuthorizationGrantType(microservicesAuthClient.getAuthorizationGrantType()))
+                .redirectUri(microservicesAuthClient.getRedirectUri())
+                .scope(microservicesAuthClient.getScope())
+                .authorizationUri(keycloak.getAuthorizationUri())
+                .tokenUri(keycloak.getTokenUri())
+                .userInfoUri(keycloak.getUserInfoUri())
+//                .userNameAttributeName(IdTokenClaimNames.SUB)
+                .userNameAttributeName(keycloak.getUserNameAttribute())
+                .jwkSetUri(keycloak.getJwkSetUri())
+                .clientName(microservicesAuthClient.getClientName())
                 .build();
     }
 
---
+++ src/main/java/com/eric6166/gateway/conroller/GatewayController.java
@@ -0,0 +1,30 @@
+package com.eric6166.gateway.conroller;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class GatewayController {
+
+//    OAuth2AuthorizedClient oAuth2AuthorizedClient;
+
+    @GetMapping(value = "/token")
+    public Mono<String> getToken(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
+        return Mono.just(authorizedClient.getAccessToken().getTokenValue());
+    }
+
+//    @GetMapping(value = "/token2")
+//    public Mono<String> getToken2(OAuth2AuthorizedClient authorizedClient) {
+//        return Mono.just(authorizedClient.getAccessToken().getTokenValue());
+//    }
+}
