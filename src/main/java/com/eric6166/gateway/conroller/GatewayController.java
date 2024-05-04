63d14fc6add238417d1af62596e15eabeaedaeba
eric6166 <ericnguyen6166@gmail.com>
2024-05-04 14:20:53 +0700

    commit

--- src/main/java/com/eric6166/gateway/conroller/GatewayController.java
+++ src/main/java/com/eric6166/gateway/conroller/GatewayController.java
@@ -1,28 +1,28 @@
-package com.eric6166.gateway.conroller;
-
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
-import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
-
-@RestController
-@RequestMapping("/auth")
-@RequiredArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-public class GatewayController {
-
-//    OAuth2AuthorizedClient oAuth2AuthorizedClient;
-
-    @PostMapping(value = "/token")
-    public Mono<String> getToken() {
-        return Mono.just("");
-    }
-
-//    @GetMapping(value = "/token2")
-//    public Mono<String> getToken2(OAuth2AuthorizedClient authorizedClient) {
-//        return Mono.just(authorizedClient.getAccessToken().getTokenValue());
-//    }
-}
+//package com.eric6166.gateway.conroller;
+//
+//import lombok.AccessLevel;
+//import lombok.RequiredArgsConstructor;
+//import lombok.experimental.FieldDefaults;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import reactor.core.publisher.Mono;
+//
+//@RestController
+//@RequestMapping("/auth")
+//@RequiredArgsConstructor
+//@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+//public class GatewayController {
+//
+////    OAuth2AuthorizedClient oAuth2AuthorizedClient;
+//
+////    @PostMapping(value = "/token")
+////    public Mono<String> getToken() {
+////        return Mono.just("");
+////    }
+//
+////    @GetMapping(value = "/token2")
+////    public Mono<String> getToken2(OAuth2AuthorizedClient authorizedClient) {
+////        return Mono.just(authorizedClient.getAccessToken().getTokenValue());
+////    }
+//}
